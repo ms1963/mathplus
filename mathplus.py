3b200bb9860f3400c59b9154a65ae6fb5f400644
Michael Stal <michael.stal@gmail.com>
2022-11-14 14:05:30 +0100

    license added as header

--- mathplus.py
+++ mathplus.py
@@ -1,4 +1,11 @@
 
+"""
+mathplus.py is licensed under the
+GNU General Public License v3.0
+
+Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+"""
+
 ##############################################################
 # This is an implementation of Matrix and Vector datatypes 
 # in Python. In addition, classes for rational numbers and 
