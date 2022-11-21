c14cafc72d10ab4531f3bcc28297f899ad2582e6
Michael Stal <michael.stal@gmail.com>
2022-11-21 18:10:05 +0100

    changed import statement

--- mathplus-demo.py
+++ mathplus-demo.py
@@ -7,8 +7,8 @@ GNU General Public License v3.0
 Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
 """
 
+from mathplus import *
 
-from mathplus import Array, Vector, Matrix, Common, Polynomial, Rational, Regression, Clustering, Newton, FunctionMatrix, Transfer
 
 import math
 
