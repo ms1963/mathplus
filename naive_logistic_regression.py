54399e0f564ad61e1c6e3cadbe29b7bef19e4d15
Michael Stal <michael.stal@gmail.com>
2022-12-21 13:07:01 +0100

    beautifications

--- naive_logistic_regression.py
+++ naive_logistic_regression.py
@@ -20,49 +20,56 @@ from mathplus import *
 X = array([[1, 2], [2, 3], [3, 4], [4, 5]])
 y = array([0, 0, 1, 1])
 
+class naive_logistic_regression:
+    def __init__(self):
+        self.weights = None
+        self.sigmoid = Utils.make_vfunc(naive_logistic_regression.sigmoid)
 
-# the hypothesis is calculated using the sigmoid function
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    # the hypothesis is calculated using the sigmoid function
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+
+    # training cycle
+    # alpha is the learning rate 
+    # X the input samples (each sample is a row of X)
+    # y is the array of ground truths. For data sample
+    # x[i] the ground truth is stored in y[i]    
+    # num_iter is the number of training iterations
+    def logistic_regression_fit(self, X, y, alpha, num_iter):
+        m, n = X.shape
+        # initialize weights with 0
+        self.weights = array.zeros((n,))
+        # iterate until max_iter is reached
+        for i in range(num_iter):
+            # X @ weights denote the inputs to sigmoid
+            z = X @ self.weights
+            # calculate sigmoid vectorized for all samples
+            # at the same time
+            predictions = self.sigmoid(z)
+            # error vector is difference between predictions and ground truth
+            error = predictions - y
+            # the gradient is X.T@error / m (number of samples) 
+            gradient = (X.T @ error).apply(lambda x: x/m)
+            # adjust weights using gradient and learning rate alpha
+            self.weights -= gradient @ alpha
+
     
-
-# create a vectorized function    
-sigmoid = Utils.make_vfunc(sigmoid)
-
-
-# training cycle
-# alpha is the learning rate 
-# X the input samples (each sample is a row of X)
-# y is the array of ground truths. For data sample
-# x[i] the ground truth is stored in y[i]    
-# num_iter is the number of training iterations
-def logistic_regression(X, y, alpha, num_iter):
-    m, n = X.shape
-    # initialize weights with 0
-    weights = array.zeros((n,))
-    # iterate until max_iter is reached
-    for i in range(num_iter):
-        # X @ weights denote the inputs to sigmoid
-        z = X @ weights
-        # calculate sigmoid vectorized for all samples
-        # at the same time
-        predictions = sigmoid(z)
-        # error vector is difference between predictions and ground truth
-        error = predictions - y
-        # the gradient is X.T@error / m (number of samples) 
-        gradient = (X.T @ error).apply(lambda x: x/m)
-        # adjust weights using gradient and learning rate alpha
-        weights -= gradient @ alpha
-    # return the weights calculated in max_iter loops
-    return weights
+    def predict(self, X, tolerance = 0.5):
+        pred = X @ self.weights
+        result = []
+        for i in range(pred.shape[0]):
+            result.append(pred[i] >= tolerance)
+        return array(result)
     
-# get the weights
-weights = logistic_regression(X, y, 0.1, 500)
+# instantiate naive_logistic_regression
+lreg = naive_logistic_regression()
+# fit the model
+lreg.logistic_regression_fit(X, y, 0.1, 1000)
 # get the prediction for a new data sample 
-test_X = array([[0.5,-0.5]])
-test_y = sigmoid(test_X @ weights)
+test_X = array([[1,1],[1,2],[4,5]])
+pred_y = lreg.predict(test_X)
 # and print it
-print(test_y)
+print(pred_y)
 
 
 
